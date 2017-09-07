5b0fdabaf511f11b6f74258c06451f934e816e7b
derick.walburger <derick.walburger@incontact.com>
2017-09-07 13:25:18 -0600

    My commit

---
+++ resources/sqsPoller.py
@@ -0,0 +1,21 @@
+import boto3
+import sys
+
+# Get the service resource
+sqs = boto3.client('sqs', region_name='us-west-2', aws_access_key_id='AKIAIGPK4KUBF4HIY7KQ', aws_secret_access_key='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')
+
+url = sqs.get_queue_url(QueueName='Showcase1.fifo')["QueueUrl"]
+
+while True:
+    messages = sqs.receive_message(QueueUrl=url, MaxNumberOfMessages=1, WaitTimeSeconds=10)
+    if 'Messages' in messages:
+        for message in messages['Messages']:
+            sqs.delete_message(QueueUrl=url,ReceiptHandle=message['ReceiptHandle'])
+
+            print(message['Body'])
+            if(message['Body'] == "True"):
+                sys.exit(1)
+            else:
+                sys.exit(0)
+    else:
+        print('Timeout - try again')
